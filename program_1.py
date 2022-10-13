52d22c7abead0adbf2430cdadd17fba2e47968cf
Aakzero <112263310+Aakzero@users.noreply.github.com>
2022-10-13 19:46:03 +0530

    Add files via upload

---
+++ program_1.py
@@ -0,0 +1,24 @@
+1. print("Hello Python")
+
+2. a=float(input("Enter 1st number "))
+   b=float(input("Enter 2nd number "))
+   c=a+b
+   d=a/b
+   print("Addition of the input numbers is",c , "\nDivision of the input numbers is",d)
+
+3. a=float(input("Enter height of the triangle "))
+   b=float(input("Enter base of the triangle "))
+   c=0.5*a*b
+   print("Area of the triangle is ",c)
+
+4. a=input("Enter a number to put it in variable a ")
+   b=input("Enter a string to put it in variable b ")
+   c=a
+   a=b
+   b=c
+   print("After swaping the variables we have ""\n",b,"in b and ""\n",a,"in a")
+
+5. import random as ran
+   a=int(input("Enter a number to output a random number below it"))
+   print(ran.randint(0,a))
+
